9b6dd738d4b4275e53c051540b46dbc86184ba04
Mihail-Ned <146471200+mihail-ned@users.noreply.github.com>
2024-04-18 02:14:41 +0200

    lesson_11

---
+++ src/main/java/com/demoqa/pages/HomePage.java
@@ -0,0 +1,53 @@
+package com.demoqa.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage{
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(css = ".top-card:nth-child(3)")
+    WebElement alertsFrameWindows;
+
+    public SidePanel getAlertsFrameWindows() {
+        //click(alertsFrameWindows); // у кого большой экран монитора
+        clickWithJS(alertsFrameWindows, 0, 300);
+        return new SidePanel(driver);
+
+    }
+
+
+    @FindBy(css = ".top-card:nth-child(4)")
+    WebElement widgets ;
+
+    public SidePanel getWidgets() {
+        //click(widgets); // у кого большой экран монитора
+        clickWithJS(widgets, 0, 300 );
+        return new SidePanel(driver);
+    }
+
+    //
+    @FindBy(css = ".top-card:nth-child(2)")
+    WebElement forms ;
+
+    public SidePanel getForms() {
+        //click(forms); // у кого большой экран монитора
+        clickWithJS(forms, 0, 300 ); // у кого маленький экран монитора
+        return new SidePanel(driver);
+    }
+
+    //
+
+    @FindBy(css = ".top-card:nth-child(5)")
+    WebElement interactions;
+
+    public SidePanel getInteractions() {
+        //click(interactions); // у кого большой экран монитора
+        clickWithJS(interactions, 0, 300 );
+        return new SidePanel(driver);
+    }
+}//class
